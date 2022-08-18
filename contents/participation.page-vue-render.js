5ed0d6563e117168506bb18dccc5218ddc2eba7c
Damith C. Rajapakse <damith@gmail.com>
2022-08-18 11:47:14 +0800

    Site Update. [skip ci]

--- contents/participation.page-vue-render.js
+++ contents/participation.page-vue-render.js
@@ -2786,6 +2786,6 @@ with(this){return _c('td',[_c('span',{staticClass:"badge bg-success mr-1"},[_v("
 with(this){return _c('td',[_v("W1:"),_c('span',{staticClass:"badge bg-light text-success"},[_v("S")]),_v(" "),_c('span',{staticClass:"badge bg-light text-success"},[_v("Q")])])}
 },function anonymous(
 ) {
-with(this){return _c('footer',[_c('div',{staticClass:"text-center"},[_c('small',[_v("["),_c('span',[_c('strong',[_v("Powered by")])]),_v(" "),_c('img',{attrs:{"src":"https://markbind.org/favicon.ico","width":"30"}}),_v(" "),_c('a',{attrs:{"href":"https://markbind.org/"}},[_v("MarkBind 4.0.0")]),_v(", generated on Thu, 18 Aug 2022, 0:06:44 GMT+8]")])])])}
+with(this){return _c('footer',[_c('div',{staticClass:"text-center"},[_c('small',[_v("["),_c('span',[_c('strong',[_v("Powered by")])]),_v(" "),_c('img',{attrs:{"src":"https://markbind.org/favicon.ico","width":"30"}}),_v(" "),_c('a',{attrs:{"href":"https://markbind.org/"}},[_v("MarkBind 4.0.0")]),_v(", generated on Thu, 18 Aug 2022, 11:46:59 GMT+8]")])])])}
 }];
   
